55b1c3ee4735e8cab2e64113c0eaae0b006deffd
syltensylten <jono0339@ad.umu.se>
2025-03-22 10:35:59 +0100

    scripts

--- scripts/vid2cover_gif.sh
+++ scripts/vid2cover_gif.sh
@@ -9,11 +9,14 @@ fi
 input_file=$1
 start_time=00:$2
 end_time=00:$3
+output_file=$4
 
 echo "$start_time $end_time"
 
-output_file="${input_file%.*}.gif"
+if [ z- "$output_file" ]; then
+  output_file="${input_file%.*}.gif"
+fi
 
 ffmpeg -i "$input_file" -ss $start_time -to $end_time -vf "scale=500:500, fps=8" -c:v gif "$output_file"
 
-echo "$output_file"
+echo "gif saved as $output_file"
