43ae7a747240340a81c08b551723790012141797
syltensylten <jono0339@ad.umu.se>
2025-05-03 23:17:38 +0200

    initial commit - Vue version of syltens static wepage

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <h1>About Us</h1>
+    <p>This is your About page content.</p>
+  </section>
+</template>
+
+<script setup>
+// No JS yet, but you can add interactivity here.
+</script>
+
+<style scoped>
+section {
+  padding: 2rem;
+}
+</style>
