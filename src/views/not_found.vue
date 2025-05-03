a228057617ada4d5cf8a1f58832e664972c99a4b
syltensylten <sylt@duck.com>
2025-05-03 21:48:59 +0200

    edits

---
+++ src/views/not_found.vue
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Syltens. B</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="img/syltensb.png" type="image/png">
+</head>
+<body>
+    <h1>404</h1>
+    <h2>Page not found</h2>
+    <a href="index.html" class="notfound-icon"><img src="img/syltensb.png" alt="home"></a>
+</body>
